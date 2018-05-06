69eaf9fb6bc1d666a83300b1a56c93d7ea4626c7
Rabinlamichhane1606 <38200740+Rabinlamichhane1606@users.noreply.github.com>
2018-05-05 22:14:53 -0700

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+
+int ch;
+printf("Enter a number");
+scanf("%d",&ch);
+switch(ch)
+{
+	case 1:
+	printf("sunday");
+		break;
+	case 2:
+		printf("Monday");
+		break;
+	case 3:
+		printf("Tuesday");
+	    break;
+	case 4:
+		printf("Wednesday");
+	    break;
+	case 5:
+		printf("THursday");
+ 		break;
+	case 6:
+  		printf("Friday");
+	    break;
+	case 7:
+		printf("Saturday");
+		break;
+					
+}
+getch();
+}
+
+
