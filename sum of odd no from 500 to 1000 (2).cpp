007a531589419bd5cac1b608637b2ab130d43a13
Rabinlamichhane1606 <38200740+Rabinlamichhane1606@users.noreply.github.com>
2018-06-02 22:39:56 -0700

    Add files via upload

---
+++ sum of odd no from 500 to 1000 (2).cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i=500,sum=0;
+	while(i<=1000)
+	{
+		i++;
+		sum=sum+i;
+	}
+	printf("the sum is %d",sum);
+getch();
+}
---
+++ while loop.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i=0;
+	while(i<=100)
+	{
+		printf("%d\n",i);
+		i++;
+	}
+	getch();
+	return(0);
+}
